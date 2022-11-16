3e7f5d56fa14d984ec7bf3654e5548f5ad862640
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-11-16 12:46:27 +0200

    task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,54 @@
 import './css/styles.css';
+import NewsApiService from './fetchSearchQuery';
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
+const formSubmitRefs = document.querySelector('#search-form');
+const articlesContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const newsApiService = new NewsApiService();
+// loadMoreBtn.disabled = true;
 
-// API_KEY 31315876-ebaad9cfb6f2dd991d80baf37
+console.log(formSubmitRefs);
+
+formSubmitRefs.addEventListener('submit', onClickSearchBtn);
+// loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onClickSearchBtn(e) {
+  e.preventDefault();
+   newsApiService.query= e.currentTarget.elements.searchQuery.value;
+  console.log(newsApiService.query);
+
+  newsApiService.fetchSearchQuery().then(renderQueryCards);
+}
+
+function renderQueryCards(querys) {
+  const markup = querys
+    .map(({ hits }) => {
+
+      return `<div class="photo-card">
+  <img src="${hits.webformatURL}" alt="${hits.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes${hits.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join('');
+
+  articlesContainer.insertAdjacentHTML('afterbegin', markup);
+}
+
+function onLoadMore () {
+    newsApiService.fetchSearchQuery(searchQuery)
+}

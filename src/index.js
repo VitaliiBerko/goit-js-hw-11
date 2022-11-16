443668cdc572262a412c4be7b29a0f0f5b8a622e
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-11-16 14:42:56 +0200

    task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import NewsApiService from './fetchSearchQuery';
+import NewsApiService from './newsApiService';
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
@@ -7,30 +7,33 @@ const formSubmitRefs = document.querySelector('#search-form');
 const articlesContainer = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 const newsApiService = new NewsApiService();
+
+
 // loadMoreBtn.disabled = true;
 
 console.log(formSubmitRefs);
 
 formSubmitRefs.addEventListener('submit', onClickSearchBtn);
 // loadMoreBtn.addEventListener('click', onLoadMore);
+// console.log(loadMoreBtn);
 
 function onClickSearchBtn(e) {
   e.preventDefault();
    newsApiService.query= e.currentTarget.elements.searchQuery.value;
+   newsApiService.resetPage();
   console.log(newsApiService.query);
 
   newsApiService.fetchSearchQuery().then(renderQueryCards);
 }
 
-function renderQueryCards(querys) {
-  const markup = querys
-    .map(({ hits }) => {
+function renderQueryCards(hits) {
+  const markup = hits.map((hit) => {
 
       return `<div class="photo-card">
-  <img src="${hits.webformatURL}" alt="${hits.tags}" loading="lazy" />
+  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes${hits.likes}</b>
+      <b>Likes${hit.likes}</b>
     </p>
     <p class="info-item">
       <b>Views</b>
@@ -50,5 +53,5 @@ function renderQueryCards(querys) {
 }
 
 function onLoadMore () {
-    newsApiService.fetchSearchQuery(searchQuery)
+    newsApiService.fetchSearchQuery()
 }
---
+++ src/newsApiService.js
@@ -0,0 +1,34 @@
+const baseUrl = '';
+import axios from 'axios';
+
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.API_KEY = '31315876-ebaad9cfb6f2dd991d80baf37';
+  }
+
+  fetchSearchQuery() {
+    return fetch(
+      `https://pixabay.com/api/?key=${this.API_KEY}&q=${this.searchQuery}&image_type=photo&page=${this.page}`
+    ).then(r=>r.json()).then(data => {
+      this.incrementPage();
+      return data.hits;
+    });
+  }
+  incrementPage() {
+    this.page = +1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+// export { NewsApiService };
